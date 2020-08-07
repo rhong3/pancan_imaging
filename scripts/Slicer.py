400f9421f23f186f975a777cacb467b7161418da
Runyu Hong <michaelhong1995@gmail.com>
2020-08-07 17:00:01 -0400

    8.7.2

--- scripts/Slicer.py
+++ scripts/Slicer.py
@@ -83,9 +83,9 @@ def v_slide(slp, n_y, x, y, tile_size, stepsize, x0, outdir, level, dp, std):
 # First open the slide, determine how many tiles can be cut, record the residue edges width,
 # and calculate the final output prediction heat map size should be. Then, using multithread to cut tiles, and stack up
 # tiles and their position dictionaries.
-def tile(image_file, outdir, level, std_img, path_to_slide="../images/", dp=None, ft=1):
-    slide = OpenSlide(path_to_slide+image_file)
-    slp = str(path_to_slide+image_file)
+def tile(image_file, outdir, level, std_img, dp=None, ft=1):
+    slide = OpenSlide(image_file)
+    slp = str(image_file)
     print(slp)
     print(slide.level_dimensions)
 
