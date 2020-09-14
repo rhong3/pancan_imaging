0470c462909914907b10cdcd9f79dc7bed742a8d
rhong3 <michaelhong1995@gmail.com>
2020-09-14 10:55:47 -0400

    9.14.3

---
+++ scripts/check_intersect.py
@@ -0,0 +1,16 @@
+# Check for slides that are not in Data freeze; in Data freeze but not in folder
+import pandas as pd
+
+df = pd.read_csv('../data_freeze.tsv', header=0, sep='\t')
+tile = pd.read_csv('../tiles_summary.csv', header=0)
+
+df_list = df['Slide_ID'].tolist()
+tile_not_in = tile[~tile['slide'].isin(df_list)]
+tile_not_in = tile_not_in.to_csv('../not_DF_tiles_summary.csv')
+tile_in = tile[tile['slide'].isin(df_list)]
+tile_in = tile_in.to_csv('../DF_tiles_summary.csv')
+
+tile_list = tile['slide'].tolist()
+df_not_in = df[~df['Slide_ID'].isin(tile_list)]
+print(df_not_in)
+
