80d368d06a1657fb92985fc025ea426916c9e2ea
rhong3 <michaelhong1995@gmail.com>
2020-10-23 20:10:33 -0400

    10.23.1

--- scripts/Statistics_mutation.R
+++ scripts/Statistics_mutation.R
@@ -8,7 +8,7 @@ library(MLmetrics)
 library(boot)
 library(gmodels)
 
-inlist = c('TP53-TP53-10')
+inlist = c('PIK3CA-PIK3CA-10')
 # Check previously calculated trials
 previous=read.csv("~/Documents/pancan_imaging/Results/Statistics_mutation.csv")
 existed=paste(previous$Folder, previous$Gene, previous$Type_number, sep='-')
