86b820d2057f4f2d98778282cecf9927d09c3bcc
rhong3 <michaelhong1995@gmail.com>
2021-09-16 10:42:56 -0400

    9.16.1

---
+++ scripts/CKA.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+def unbiased_HSIC(K, L):
+    '''Computes an unbiased estimator of HISC. This is equation (2) from the paper'''
+
+    # create the unit **vector** filled with ones
+    n = K.shape[0]
+    ones = np.ones(shape=(n))
+
+    # fill the diagonal entries with zeros
+    np.fill_diagonal(K, val=0)  # this is now K_tilde
+    np.fill_diagonal(L, val=0)  # this is now L_tilde
+
+    # first part in the square brackets
+    trace = np.trace(np.dot(K, L))
+
+    # middle part in the square brackets
+    nominator1 = np.dot(np.dot(ones.T, K), ones)
+    nominator2 = np.dot(np.dot(ones.T, L), ones)
+    denominator = (n - 1) * (n - 2)
+    middle = np.dot(nominator1, nominator2) / denominator
+
+    # third part in the square brackets
+    multiplier1 = 2 / (n - 2)
+    multiplier2 = np.dot(np.dot(ones.T, K), np.dot(L, ones))
+    last = multiplier1 * multiplier2
+
+    # complete equation
+    unbiased_hsic = 1 / (n * (n - 3)) * (trace + middle - last)
+
+    return unbiased_hsic
+
+
+def CKA(X, Y):
+    '''Computes the CKA of two matrices. This is equation (1) from the paper'''
+
+    nominator = unbiased_HSIC(np.dot(X, X.T), np.dot(Y, Y.T))
+    denominator1 = unbiased_HSIC(np.dot(X, X.T), np.dot(X, X.T))
+    denominator2 = unbiased_HSIC(np.dot(Y, Y.T), np.dot(Y, Y.T))
+
+    cka = nominator / np.sqrt(denominator1 * denominator2)
+
+    return cka
+
+
+def calculate_CKA_for_two_matrices(activationA, activationB):
+  '''Takes two activations A and B and computes the linear CKA to measure their similarity'''
+
+  #unfold the activations, that is make a (n, h*w*c) representation
+  shape = activationA.shape
+  activationA = np.reshape(activationA, newshape=(shape[0], np.prod(shape[1:])))
+
+  shape = activationB.shape
+  activationB = np.reshape(activationB, newshape=(shape[0], np.prod(shape[1:])))
+
+  #calculate the CKA score
+  cka_score = CKA(activationA, activationB)
+
+  del activationA
+  del activationB
+
+  return cka_score
+
+
