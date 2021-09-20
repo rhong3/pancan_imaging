792b897567cb6b637208a3c934fc230017fffc6d
rhong3 <michaelhong1995@gmail.com>
2021-09-20 14:48:15 -0400

    9.20.2

--- scripts/CAM_giant.py
+++ scripts/CAM_giant.py
@@ -10,38 +10,58 @@ import pandas as pd
 import cv2
 import argparse
 import os
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dirr', type=str, default='trial', help='output directory')
+parser.add_argument('--mode', type=str, default='ol', help='heatmap only or overlay')
+parser.add_argument('--slide', type=str, help='slide name to plot')
+
 opt = parser.parse_args()
 dirr = opt.dirr
+mode = opt.mode
 
 
-hmpd = pd.DataFrame(columns=['idx', 'bool', 'hmpath', 'level'])
-olpd = pd.DataFrame(columns=['idx', 'bool', 'olpath', 'level'])
 # Map output CAMs to dict
-for lv in range(1, 4):
-    hm = []
-    ol = []
-    for it in os.listdir('../Results/'+dirr+'/out/Test_level'+str(lv)+'_img'):
-        if '_hm.png' in it:
-            hm.append([it.split('_')[0], it.split('_')[1], it])
-        elif '_ol.png' in it:
-            ol.append([it.split('_')[0], it.split('_')[1], it])
-    hm = pd.DataFrame(hm, columns=['idx', 'bool', 'hmpath'])
-    hm['level'] = lv
-    ol = pd.DataFrame(ol, columns=['idx', 'bool', 'olpath'])
-    ol['level'] = lv
-    hmpd.append(hm)
-    olpd.append(ol)
+hm = []
+ol = []
+for it in os.listdir('../Results/'+dirr+'/out/Test_level1_img'):
+    if '_hm.png' in it:
+        hm.append([it.split('_')[0], it.split('_')[1], '../Results/'+dirr+'/out/Test_level1_img/'+it,
+                   '../Results/'+dirr+'/out/Test_level2_img/'+it, '../Results/'+dirr+'/out/Test_level3_img/'+it])
+    elif '_ol.png' in it:
+        ol.append([it.split('_')[0], it.split('_')[1], '../Results/'+dirr+'/out/Test_level1_img/'+it,
+                   '../Results/'+dirr+'/out/Test_level2_img/'+it, '../Results/'+dirr+'/out/Test_level3_img/'+it])
 
-pdpd = hmpd.join(olpd.set_index('idx'))
-tiles = pd.read_csv('../Results/'+dirr+'/out/Test_tile.csv')
+hm = pd.DataFrame(hm, columns=['idx', 'bool', 'caml1path', 'caml2path', 'caml3path'])
+ol = pd.DataFrame(ol, columns=['idx', 'bool', 'caml1path', 'caml2path', 'caml3path'])
+
+tiles = pd.read_csv('../Results/'+dirr+'/out/Test_tile.csv', header=0)
 tiles['idx'] = tiles.index
 
-tiles = tiles.join()
+hmtiles = tiles.join(hm.set_index('idx'), on='idx', how='inner')
+oltiles = tiles.join(ol.set_index('idx'), on='idx', how='inner')
+hmtiles.to_csv('../Results/'+dirr+'/out/hmtiles.csv', index=False)
+oltiles.to_csv('../Results/'+dirr+'/out/oltiles.csv', index=False)
 
 
 # Create giant image from dict
+if mode == "hm":
+    tdict = hmtiles
+else:
+    tdict = oltiles
+tdict = tdict[(tdict['bool'] == 'Correct') & tdict['Slide_ID'] == opt.slide]
+tm = tdict.iloc[0, 2]
+pt = tdict.iloc[0, 0]
 
+for j in range(1, 4):
+    idict = pd.read_csv('../tiles/'+tm+'/'+pt+'/'+opt.slide.split('-')[-1]+'/level'+str(j)+'/dict.csv', header=0)
+    canvas = np.full((idict['X_pos'].max()*50+50, idict['Y_pos'].max()*50+50, 3), 0)
+    for idx, row in tdict.iterrows():
+        x = int(row['L1path'].split('x-')[1].split('-y')[0])/10/(2**(j-1))
+        y = int(row['L1path'].split('y-')[1].split('.pn')[0])/10/(2**(j-1))
+        imm = cv2.imread(row['caml1path'])[25:275, 25:275, :]
+        imm = cv2.resize(imm, (50, 50), interpolation=cv2.INTER_AREA)
+        canvas[x:x+50, y:y+50, :] = imm
+    cv2.imwrite('../Results/'+dirr+'/out/'+mode+'_level'+str(j)+'.jpeg', canvas)
 
