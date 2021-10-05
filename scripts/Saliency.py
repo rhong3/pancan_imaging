a1cc868cd91bcbbfa2bfcc17ac3e2a752392b96c
rhong3 <michaelhong1995@gmail.com>
2021-10-05 11:58:16 -0400

    10.5.1

--- scripts/Saliency.py
+++ scripts/Saliency.py
@@ -13,8 +13,7 @@ import cv2
 import pandas as pd
 import tensorflow as tf
 import Panoptes1
-import saliency.tf1 as saliency
-from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+import saliency
 
 
 parser = argparse.ArgumentParser()
@@ -58,12 +57,12 @@ if __name__ == "__main__":
         # image input
         xa_in = tf.placeholder(tf.float32, name="xa")
         xa_in_reshape = tf.reshape(xa_in, [-1, 299, 299, 3])
-        xb_in = tf.placeholder(tf.float32, name="xb")
-        xb_in_reshape = tf.reshape(xb_in, [-1, 299, 299, 3])
-        xc_in = tf.placeholder(tf.float32, name="xc")
-        xc_in_reshape = tf.reshape(xc_in, [-1, 299, 299, 3])
+        # xb_in = tf.placeholder(tf.float32, name="xb")
+        # xb_in_reshape = tf.reshape(xb_in, [-1, 299, 299, 3])
+        # xc_in = tf.placeholder(tf.float32, name="xc")
+        # xc_in_reshape = tf.reshape(xc_in, [-1, 299, 299, 3])
 
-        logits, nett = inference(xa_in_reshape, xb_in_reshape, xc_in_reshape, classes)
+        logits, nett = inference(xa_in_reshape, xa_in_reshape, xa_in_reshape, classes)
         pred = tf.nn.softmax(logits, name="prediction")
         neuron_selector = tf.placeholder(tf.int32)
         y = logits[0][neuron_selector]
@@ -80,53 +79,49 @@ if __name__ == "__main__":
                 pass
             test_tiles = pd.read_csv('../Results/'+dirr+'/out/Test_tile.csv')
             for idx, row in test_tiles.iterrows():
-                for diiir in [str('../Results/' + dirr + '/saliency/' + str(row['Slide_ID'])),
-                              str('../Results/' + dirr + '/saliency/' + str(row['Slide_ID']) + '/level1'),
-                              str('../Results/' + dirr + '/saliency/' + str(row['Slide_ID']) + '/level2'),
-                              str('../Results/' + dirr + '/saliency/' + str(row['Slide_ID']) + '/level3')]:
-                    try:
-                        os.mkdir(dirpath)
-                    except FileExistsError:
-                        pass
+                print(row)
+                try:
+                    os.mkdir(dirpath + str(row['Slide_ID']))
+                except FileExistsError:
+                    pass
                 # prediction_class = sess.run(
                 #     [prediction], {x_in: img})[0]
                 # weight = sess.run('logits/logits/weights:0')
 
                 # Baseline is a white image.
-                for paths in ['L1path', 'L2path', 'L3path']:
-                    grad = saliency.IntegratedGradients(graph, sess, y, xa_in_reshape)
-                    img = cv2.imread(row[paths])
-                    img = img.astype(np.float32)
-                    baseline = np.zeros(img.shape)
-                    baseline.fill(255)
-                    # vanilla_mask_3d = grad.GetMask(img, feed_dict={neuron_selector: 1},
-                    #                                x_stepIntegratedGradientss=25, x_baseline=baseline)
-                    smoothgrad_mask_3d = grad.GetSmoothedMask(img, feed_dict={
-                        neuron_selector: int(row["label"])}, x_steps=5, x_baseline=baseline, batch_size=1)
+                grad = saliency.IntegratedGradients(graph, sess, y, xa_in_reshape)
+                img = cv2.imread(row['L1path'])
+                img = img.astype(np.float32)
+                baseline = np.zeros(img.shape)
+                baseline.fill(255)
+                # vanilla_mask_3d = grad.GetMask(img, feed_dict={neuron_selector: 1},
+                #                                x_stepIntegratedGradientss=25, x_baseline=baseline)
+                smoothgrad_mask_3d = grad.GetSmoothedMask(img, feed_dict={
+                    neuron_selector: int(row["label"])}, x_steps=5, x_baseline=baseline)
 
-                    # Call the visualization methods to convert the 3D tensors to 2D grayscale.
-                    # vanilla_mask_grayscale = saliency.VisualizeImageGrayscale(vanilla_mask_3d)
-                    smoothgrad_mask_grayscale = saliency.VisualizeImageGrayscale(smoothgrad_mask_3d)
+                # Call the visualization methods to convert the 3D tensors to 2D grayscale.
+                # vanilla_mask_grayscale = saliency.VisualizeImageGrayscale(vanilla_mask_3d)
+                smoothgrad_mask_grayscale = saliency.VisualizeImageGrayscale(smoothgrad_mask_3d)
 
-                    print(row['Slide_ID'])
-                    # vanilla_mask_grayscale = im2double(vanilla_mask_grayscale)
-                    # vanilla_mask_grayscale = py_map2jpg(vanilla_mask_grayscale)
-                    # a = im2double(img) * 255
-                    # b = im2double(vanilla_mask_grayscale) * 255
-                    # curHeatMap = a * 0.5 + b * 0.5
-                    # ab = np.hstack((a, b))
-                    # full = np.hstack((curHeatMap, ab))
-                    # cv2.imwrite(str(dirpath + '/' + aa), full)
+                print(row['Slide_ID'])
+                # vanilla_mask_grayscale = im2double(vanilla_mask_grayscale)
+                # vanilla_mask_grayscale = py_map2jpg(vanilla_mask_grayscale)
+                # a = im2double(img) * 255
+                # b = im2double(vanilla_mask_grayscale) * 255
+                # curHeatMap = a * 0.5 + b * 0.5
+                # ab = np.hstack((a, b))
+                # full = np.hstack((curHeatMap, ab))
+                # cv2.imwrite(str(dirpath + '/' + aa), full)
 
-                    smoothgrad_mask_grayscale = im2double(smoothgrad_mask_grayscale)
-                    smoothgrad_mask_grayscale = py_map2jpg(smoothgrad_mask_grayscale)
-                    sa = im2double(img) * 255
-                    sb = im2double(smoothgrad_mask_grayscale) * 255
-                    scurHeatMap = sa * 0.5 + sb * 0.5
-                    sab = np.hstack((sa, sb))
-                    sfull = np.hstack((scurHeatMap, sab))
+                smoothgrad_mask_grayscale = im2double(smoothgrad_mask_grayscale)
+                smoothgrad_mask_grayscale = py_map2jpg(smoothgrad_mask_grayscale)
+                sa = im2double(img) * 255
+                sb = im2double(smoothgrad_mask_grayscale) * 255
+                scurHeatMap = sa * 0.5 + sb * 0.5
+                sab = np.hstack((sa, sb))
+                sfull = np.hstack((scurHeatMap, sab))
 
-                    cv2.imwrite(str(dirpath+str(row["Slide_ID"])+"/"+row[paths].split("/", 5)), sfull)
+                cv2.imwrite(str(dirpath+str(row["Slide_ID"])+"/"+row['L1path'].split("/", 5)[-1]), sfull)
 
     # print(np.shape(x_))
     # print(np.shape(nett_))
