e9426d850d3399d9d11332aabe7342384eaf389e
rhong3 <michaelhong1995@gmail.com>
2022-03-08 17:26:31 -0500

    3.8.2

--- scripts/tSNE_sampling.py
+++ scripts/tSNE_sampling.py
@@ -7,6 +7,10 @@ for i in ['IC46', 'IC29', 'IC38', 'IC15', 'IC3']:
     n = sum(1 for line in open('../Results/{}/out/For_tSNE.csv'.format(i))) - 1
     s = 20000
     skip = sorted(random.sample(range(n), n - s))
+    try:
+        skip.remove(0)
+    except:
+        pass
     xx = pd.read_csv('../Results/{}/out/For_tSNE.csv'.format(i), header=0, skiprows=skip)
     xx.to_csv('../Results/{}/out/tSNE_sampled.csv'.format(i), index=False)
 
